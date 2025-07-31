48886f2494e19b449672af874eb6d619a9faff9b
ecesevval23 <ecesevvalbingol@gmail.com>
2025-07-31 13:45:10 +0300

    fizik eklendi

--- Assets/Codes/PlayerController.cs
+++ Assets/Codes/PlayerController.cs
@@ -1,24 +1,49 @@
 using System.Collections;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public CharacterController controller; // Reference to the CharacterController component
-    public float speed = 12f; // Speed of the character movement
+    public CharacterController controller;     // CharacterController component'ine referans
+    public float speed = 12f;                  // Yürüme hýzý
+    public float gravity = -9.81f;             // Yerçekimi kuvveti (negatif olmalý)
+    public float jumpHeight = 3f;              // Zýplama yüksekliði
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Vector3 velocity;                  // Hareket vektörü (özellikle Y için)
+    private bool isGrounded;                   // Yere temas kontrolü
 
-    // Update is called once per frame
+    public Transform groundCheck;              // Ayak altý kontrol objesi (boþ GameObject)
+    public float groundDistance = 0.4f;        // Küre yarýçapý
+    public LayerMask groundMask;               // Sadece zeminle kontrol
+
     void Update()
     {
+        // Yere temas kontrolü
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // Yerdeysek düþüþ hýzýný sýfýrla (küçük negatif deðerle yapýþýk kalýr)
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Yön tuþlarýyla yatay hareket
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * z; // Calculate movement direction based on input
-        controller.Move(move * speed * Time.deltaTime); // Move the character based on input and speed
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Space'e basýldýðýnda zýpla (eðer yerdeysen)
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Yerçekimi uygula (her frame yavaþça düþüþ eklenir)
+        velocity.y += gravity * Time.deltaTime;
+
+        // Yukarý-aþaðý hareketi uygula
+        controller.Move(velocity * Time.deltaTime);
     }
 }
